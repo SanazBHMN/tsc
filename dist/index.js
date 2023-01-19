76401413098a09d669cb8e2a67b347c4963fae61
SanazBHMN <sanazbahmani35@gmail.com>
2023-01-19 11:17:12 +0330

    The any type

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+console.log(sales);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-let age: number = 20;
-if (age < 50) age += 10;
-console.log(age);
+// any
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+
+// not ideal if we have thousands of this error
+function render(document: any) {
+  console.log(document);
+}
