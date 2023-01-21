9e87d4ddb090ba0fc4ec58671ec77c6971f3840c
SanazBHMN <sanazbahmani35@gmail.com>
2023-01-21 12:48:53 +0330

    Enums

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,12 @@
 let sales = 123456789;
 let course = "TypeScript";
 let is_published = true;
-console.log(sales);
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+let user = [1, "Sanaz"];
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,17 @@ let numbers: number[] = [1, 2, 3];
 
 // tuples
 let user: [number, string] = [1, "Sanaz"];
+
+// enums; a list of related constants
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+const enum Size {
+  Samll = 1,
+  Medium,
+  Large,
+}
+
+let mySize: Size = Size.Medium;
+console.log(mySize);
