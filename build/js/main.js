d9bcffa9c99357d1a7c95b5c88e4dd1d86bd410c
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-08-16 19:05:59 +0330

    Aliases

--- build/js/main.js
+++ build/js/main.js
@@ -1,54 +1,24 @@
 "use strict";
-let stringArr = ['one', 'hey', 'Sanaz'];
-let guitars = ['Strat', 'Les Paul', 5150];
-let mixedData = ['EVH', 1984, true];
-stringArr[0] = 'John';
-stringArr.push('hey');
-guitars[0] = 1984;
-guitars.unshift('Jim');
-let test = [];
-let bands = [];
-bands.push('Van Halen');
-// Tuple
-let myTuple = ['Sanaz', 26, true];
-let mixed = ['John', 1, false];
-myTuple[1] = 42;
-// Objects
-let myObj;
-myObj = [];
-console.log(typeof myObj);
-myObj = bands;
-myObj = {};
-const exampleObj = {
-    prop1: 'Sanaz',
-    prop2: true
+// Literal types
+let myName;
+let username;
+username = "Amy";
+// functions
+const add = (a, b) => {
+    return a + b;
 };
-exampleObj.prop1 = 'John';
-let evh = {
-    // name: 'Eddie',
-    active: false,
-    albums: [1984, 5150, 'OU812']
+const logMessage = (message) => {
+    console.log(message);
 };
-let jp = {
-    name: 'Jimmy',
-    active: true,
-    albums: ['I', 'II', 'IV']
+logMessage('Hello');
+logMessage(add(2, 3));
+let subtract = function (c, d) {
+    return c - d;
 };
-const greetGuitarist = (guitarist) => {
-    if (guitarist.name) {
-        return `Hello ${guitarist.name.toUpperCase()}!`;
-    }
-    return 'Hello!';
+// interface mathFunction {
+//     (a: number, b: number): number
+// }
+let multiply = function (c, d) {
+    return c * d;
 };
-console.log(greetGuitarist(evh));
-// Enums
-// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime."
-var Grade;
-(function (Grade) {
-    Grade[Grade["U"] = 1] = "U";
-    Grade[Grade["D"] = 2] = "D";
-    Grade[Grade["C"] = 3] = "C";
-    Grade[Grade["B"] = 4] = "B";
-    Grade[Grade["A"] = 5] = "A";
-})(Grade || (Grade = {}));
-console.log(Grade.U);
+logMessage(multiply(2, 2));
--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,46 @@
-let stringArr = ['one', 'hey', 'Sanaz']
+// Type Aliases
+type stringOrNumber = string | number
 
-let guitars = ['Strat', 'Les Paul', 5150]
+type stringOrNumberArray = (string | number)[]
 
-let mixedData = ['EVH', 1984, true]
-
-stringArr[0] = 'John'
-stringArr.push('hey')
-
-guitars[0] = 1984
-guitars.unshift('Jim')
-
-let test = []
-let bands: string[] = []
-bands.push('Van Halen')
-
-// Tuple
-let myTuple: [string, number, boolean] = ['Sanaz', 26, true]
-
-let mixed = ['John', 1, false]
-
-myTuple[1] = 42
-
-// Objects
-let myObj: object
-myObj = []
-console.log(typeof myObj)
-myObj = bands
-myObj = {}
-
-const exampleObj = {
-    prop1: 'Sanaz',
-    prop2: true
-}
-
-exampleObj.prop1 = 'John'
-
-interface Guitarist {
+type Guitarist = {
     name?: string,
-    active?
-    : boolean,
-    albums: (string | number)[]
+    active: boolean,
+    albums: stringOrNumberArray
 }
 
-let evh: Guitarist = {
-    // name: 'Eddie',
-    active: false,
-    albums: [1984, 5150, 'OU812']
+type userId = stringOrNumber
+
+// Literal types
+let myName: 'Dave'
+
+let username: 'Dave' | 'John' | 'Amy'
+
+username = "Amy"
+
+// functions
+const add = (a: number, b: number): number => {
+    return a + b
 }
 
-let jp: Guitarist = {
-    name: 'Jimmy',
-    active: true,
-    albums: ['I', 'II', 'IV']
+const logMessage = (message: any): void => {
+    console.log(message)
 }
 
-const greetGuitarist = (guitarist: Guitarist) => {
-    if(guitarist.name) {
-        return `Hello ${guitarist.name.toUpperCase()}!`
-    }
-    return 'Hello!'
+logMessage('Hello')
+logMessage(add(2, 3))
+
+let subtract = function(c: number, d: number): number {
+    return c - d
 }
 
-console.log(greetGuitarist(evh))
+type mathFunction = (a: number, b: number) => number
+// interface mathFunction {
+//     (a: number, b: number): number
+// }
 
-// Enums
-// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime."
-
-enum Grade {
-    U = 1,
-    D,
-    C,
-    B,
-    A,
+let multiply: mathFunction = function(c, d) {
+    return c * d
 }
 
-console.log(Grade.U)
+logMessage(multiply(2, 2))
